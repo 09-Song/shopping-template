9f456c1fcfaaacbea00d979e35f3fe3678aad729
09-Song <ssong8154@gmail.com>
2021-02-20 16:39:17 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -21,6 +21,41 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+ 
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+    if (key == null || value == null) {
+        return;
+    }
+    // 전체적으로 계속업데이트되는 코드
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+
+    updateItems (items, key, value);
+
+}
+// 빙글빙글돌면서 불필요한 업데이트가 없음
+/*
+function updateItems(items, key, value){
+    items.forEach(item => {
+        if (item.dataset[key] === value) {
+            item.classList.remove('invisible');
+        } else {
+            item.classList.add('invisible');
+        }
+        
+    });
+}
+*/
+function setEventListemers(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 loadItems()
     .then(items => {
         displayItems(items);
